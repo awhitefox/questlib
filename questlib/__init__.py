6f7740611ffc78e1b1c3ecc08bc343fc3a8c32d4
awhitefox <ignat.k.150@gmail.com>
2021-11-21 16:46:54 +0300

    Add OperationType to __init__.py

--- questlib/__init__.py
+++ questlib/__init__.py
@@ -1,3 +1,3 @@
-from .variable import VariableDefinition, VariableOperation
+from .variable import VariableDefinition, VariableOperation, OperationType
 from .condition import Condition, CompareTo, ComparisonType
 from .chapter import *
