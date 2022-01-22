b2ab960e5ed486e1a1da4a088fedc178ce42c10c
awhitefox <ignat.k.150@gmail.com>
2022-01-22 19:19:59 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='questlib',
+    version='0.1',
+    author='awhitefox',
+    license='MIT',
+    packages=['questlib'],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
